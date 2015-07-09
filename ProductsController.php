1af521ef63d84e2f85cd6039fd95a0bf31007376
unknown <prasad.k@ETG144.etggs.net>
2015-07-09 19:19:02 +0530

    First Commit

---
+++ ProductsController.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+App::uses('AppController', 'Controller');
+
+class ProductsController extends AppController {
+
+    public $helpers = array('Html', 'Form');
+    public $components = array('Session', 'Paginator');
+    public $paginate = array(
+        'limit' => 10
+    );
+
+    public function index() {
+        $this->Product->recursive = -1;
+        $this->set('products', $this->paginate());
+    }
+
+    public function view($id) {
+        if (!($product = $this->Product->findById($id))) {
+            throw new NotFoundException(__('Product not found'));
+        }
+        $this->set(compact('product'));
+    }
+
+}
